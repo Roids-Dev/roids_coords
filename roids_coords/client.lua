d9db7e41d2be28696dd2b3a45f3687a5ab1fd659
Roids-Dev <73410725+Roids-Dev@users.noreply.github.com>
2020-11-18 17:01:32 -0600

    Update client.lua

---
+++ roids_coords/client.lua
@@ -0,0 +1,27 @@
+
+
+
+Citizen.CreateThread(function()
+while true do
+        local player = PlayerPedId()
+        local coords = GetEntityCoords(player)
+        local heading = GetEntityHeading(player)
+        DrawTxt('Coords: '..coords..' Heading: '..heading, 0.50, 0.95, 0.7, 0.7, true, 255, 255, 255, 255, true)
+     end
+end)
+
+
+
+
+
+
+
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+    local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
+   SetTextScale(w, h)
+   SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+   SetTextCentre(centre)
+   if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+   Citizen.InvokeNative(0xADA9255D, 10);
+   DisplayText(str, x, y)
+end
